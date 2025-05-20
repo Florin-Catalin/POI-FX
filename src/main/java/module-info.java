335986a516bb4efc2cf66a41c813d77b314ee8fb
Florin-Catalin <florin.g.ungureanu@gmail.com>
2025-05-20 16:42:38 +0300

    initial javafx app

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module ufc.ptt.pttfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens ufc.ptt.pttfx to javafx.fxml;
+    exports ufc.ptt.pttfx;
+}
